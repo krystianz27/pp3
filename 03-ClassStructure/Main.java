a6659ae436e2a12d3a286a161e33766d634bdfe3
s-013-27 <s-013-27@gumiforest.ae.krakow.pl>
2023-10-24 11:15:47 +0200

    Cwiczenia 24-10-2023

---
+++ 03-ClassStructure/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Math.max(34, 39));
+        System.out.println(Math.PI);
+        System.out.println(Math.abs(-17));
+        System.out.println(Math.sqrt(9 / 2.0));
+        System.out.println(Math.random());
+        System.out.println((int)(Math.random()*11));
+        System.out.println(Math.sin(Math.toRadians(90)));
+    }
+}
+
+
+
+
+// a.	The larger of two numbers: 34 and 49
+// b.	PI number
+// c.	Absolute value of -17
+// d.	Square root of the expression: nine divided by two
+// e.	Random number in the range <0.0,1.0)
+// f.	Integer random number in the range <0,10>
+// g.	Sine of 90 degrees
